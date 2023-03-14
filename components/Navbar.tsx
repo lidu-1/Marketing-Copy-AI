73f8b014b966c577af45a222265d2a8984ab1329
Ermias Bahru <ermiasbahru878@gmail.com>
2023-03-14 17:51:10 +0300

    added components

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import Feature from "@/components/Feature";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,10 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-5xl text-center font-bold text-indigo-500 underline">
-          Hello World
-        </h1>
+      <main className="bg-gray-100 dark:bg-slate-900 min-h-screen">
+        <div className="max-w-5xl w-11/12 mx-auto px-3">
+          <Navbar />
+          <Hero />
+          <Feature />
+          <Footer />
+        </div>
       </main>
     </>
   );
