4e7497835642ad931ec5288b0452cf5c9902f5a6
lidu-1 <77609957+lidu-1@users.noreply.github.com>
2023-03-14 18:09:37 +0300

    export
    make the component export easier

---
+++ components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar";
+export { default as Hero } from "./Hero";
+export { default as Feature } from "./Feature";
+export { default as Footer } from "./Footer";
