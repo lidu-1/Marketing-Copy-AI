47ee3c1e8fb1ab074107d6462f520ab9903bf9fe
Ermias Bahru <ermiasbahru878@gmail.com>
2023-03-17 15:46:01 +0300

    added feature list component

---
+++ components/FeatureItem.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  title: string;
+  icon: JSX.Element;
+  description: string;
+};
+
+const FeatureItem = ({ title, icon, description }: Props) => {
+  return (
+    <div className="bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 transform transition rounded-md p-5">
+      <div className="text-gray-800 dark:text-gray-300 w-6 h-6 mb-3">
+        {icon}
+      </div>
+      <h1 className="font-semibold text-lg text-gray-600 dark:text-gray-200 mb-1">
+        {title}
+      </h1>
+      <p className="text-md text-gray-600 dark:text-gray-300">{description}</p>
+    </div>
+  );
+};
+
+export default FeatureItem;
